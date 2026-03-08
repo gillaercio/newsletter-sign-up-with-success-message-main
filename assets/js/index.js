4a9a16648ab388941c5e537d1414eb12fe23236f
Gildman Laércio <laerciokarlagabm@gmail.com>
2026-03-08 12:33:05 -0300

    buttons' interactivity created

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ const image = document.querySelector(".newsletter__image");
 const card = document.querySelector(".newsletter__card");
 const errorMessage = document.getElementById("error-email");
 const email = document.getElementById("form-email");
-const success = document.querySelector("newsletter__success");
+const success = document.querySelector(".is-hidden");
 
 
 function sendButton(event) {
@@ -17,7 +17,8 @@ function sendButton(event) {
   if (email.value.trim() === "") {
     errorMessage.textContent = required;
     errorMessage.classList.add("input-error");
-    email.style.border = "2px solid var(--Red)";
+    email.style.border = "1px solid var(--Red)";
+    email.style.backgroundColor = "var(--Red-button)";
     hasError = true;
   } else if (!emailRegex.test(email.value.trim())) {
     errorMessage.textContent = errorEmail;
@@ -26,4 +27,28 @@ function sendButton(event) {
     hasError = true;
   }
 
+  if (!hasError) {
+    image.style.display = "none";
+    card.style.display = "none";
+    success.style.display = "grid";
+
+    const addressEmail = document.querySelector(".newsletter__description span");
+    addressEmail.textContent = email.value;
+  }
+}
+
+function backButton(event) {
+  event.preventDefault()
+
+  image.style.display = "grid";
+  card.style.display = "grid";
+  success.style.display = "none";
+  errorMessage.style.display = "none";
+  errorMessage.classList.remove("input-error");
+  email.value = "";
+  email.classList.remove("input-error");
+  email.style.border = "1px solid var(--Grey)";
+  email.style.backgroundColor = "1px solid var(--White)";
+
+  hasError = false;
 }
