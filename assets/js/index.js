67e96ea42a0faf99d98258e87ad05b29c998bbdb
Gildman Laércio <laerciokarlagabm@gmail.com>
2026-03-15 21:26:31 -0300

    index.js fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,8 @@ const image = document.querySelector(".newsletter__image");
 const card = document.querySelector(".newsletter__card");
 const errorMessage = document.getElementById("error-email");
 const email = document.getElementById("form-email");
-const success = document.querySelector(".is-hidden");
+// const success = document.querySelector(".is-hidden");
+const success = document.querySelector(".newsletter__success");
 
 
 function sendButton(event) {
@@ -10,17 +11,21 @@ function sendButton(event) {
 
   let hasError = false;
 
+  const emailValue = email.value.trim();
+
   const required = 'This field is required';
   const errorEmail = 'Valid email required';
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (email.value.trim() === "") {
+  
+  if (!emailValue) {
+    errorMessage.classList.remove("is-hidden");
     errorMessage.textContent = required;
     errorMessage.classList.add("input-error");
-    email.style.border = "1px solid var(--Red)";
-    email.style.backgroundColor = "var(--Red-button)";
+    email.classList.remove("input-error");
     hasError = true;
-  } else if (!emailRegex.test(email.value.trim())) {
+  } else if (!emailRegex.test(emailValue)) {
+    errorMessage.classList.remove("is-hidden");
     errorMessage.textContent = errorEmail;
     errorMessage.classList.add("input-error");
     email.classList.add("input-error");
@@ -28,27 +33,33 @@ function sendButton(event) {
   }
 
   if (!hasError) {
-    image.style.display = "none";
-    card.style.display = "none";
-    success.style.display = "grid";
+    // image.style.display = "none";
+    image.classList.add("is-hidden");
+    // card.style.display = "none";
+    card.classList.add("is-hidden");
+    // success.style.display = "grid";
+    success.classList.remove("is-hidden");
 
     const addressEmail = document.querySelector(".newsletter__description span");
-    addressEmail.textContent = email.value;
+    addressEmail.textContent = emailValue;
   }
 }
 
 function backButton(event) {
   event.preventDefault()
 
-  image.style.display = "grid";
-  card.style.display = "grid";
-  success.style.display = "none";
-  errorMessage.style.display = "none";
+  // image.style.display = "grid";
+  image.classList.remove("is-hidden");
+  // card.style.display = "grid";
+  card.classList.remove("is-hidden");
+  // success.style.display = "none";
+  success.classList.add("is-hidden");
+  // errorMessage.style.display = "none";
+  errorMessage.classList.add("is-hidden");
   errorMessage.classList.remove("input-error");
   email.value = "";
   email.classList.remove("input-error");
   email.style.border = "1px solid var(--Grey)";
-  email.style.backgroundColor = "1px solid var(--White)";
-
-  hasError = false;
+  // email.style.backgroundColor = "1px solid var(--White)";
+  email.style.backgroundColor = "var(--White)";
 }
